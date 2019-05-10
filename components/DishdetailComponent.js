e67034d330be2bd77a25026bf86a145771e46a41
Luiz Cunha <luiz9495@gmail.com>
2019-05-11 02:51:54 +0900

    Animatable

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, Icon, Button, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite, postComment } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -23,6 +24,7 @@ function RenderDish(props) {
 
   if (dish != null) {
       return(
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
           <Card
           featuredTitle={dish.name}
           image={{uri: baseUrl + dish.image}}>
@@ -48,6 +50,7 @@ function RenderDish(props) {
                 />
               </View>
           </Card>
+        </Animatable.View>
       );
   }
   else {
@@ -69,7 +72,8 @@ function RenderComments(props) {
   }
 
   if (comments != null) {
-      return(
+    return(
+      <Animatable.View animation="fadeInUp" duration={2000} delay={1000}>
         <Card title='Comments'>
           <FlatList
             data={comments}
@@ -77,7 +81,8 @@ function RenderComments(props) {
             keyExtractor={item => item.id.toString()}
           />
         </Card>
-      );
+      </Animatable.View>
+    );
   }
   else {
       return(<View></View>);
