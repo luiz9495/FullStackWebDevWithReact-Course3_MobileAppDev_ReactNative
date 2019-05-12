650bca3f571560590f6734598b7ff95827177dfe
Luiz Cunha <luiz9495@gmail.com>
2019-05-12 10:10:01 +0900

    Picking an Image

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -30,7 +30,7 @@ const mapDispatchToProps = dispatch => ({
 })
 
 const LoginNavigator = createStackNavigator({
-    Login: { screen: Login }
+    Login: Login
   }, {
   navigationOptions: ({ navigation }) => ({
     headerStyle: {
